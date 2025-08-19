e3176126081677b660c65686380e11fe14154d76
kajal kumari <kajal@ kumari>
2025-08-19 22:45:19 +0530

    Resolved merge conflicts in App.jsx

--- hiremi/src/App.jsx
+++ hiremi/src/App.jsx
@@ -1,37 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Home from "./component/Home";
+import Careers from "./component/Careers";
+import Footer from "./component/Footer";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
 import Contact from "./Pages/Contact";
 
-<<<<<<< HEAD
-
 import './App.css'
-import Navbar from './component/Navbar'
-import Home from './component/Home'
-import Careers from './component/Careers'
-import Footer from './component/Footer'
-=======
->>>>>>> kk
+
 function App() {
   return (
     <>
       <Navbar />
-<<<<<<< HEAD
-      <Home />
-      <Careers />
-      <Footer />
       
-      
-=======
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<><Home /><Careers /></>} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
->>>>>>> kk
+
+      <Footer />
     </>
   );
 }
