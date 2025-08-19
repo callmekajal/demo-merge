04ac67aba7e5a9af5c24d0b095b89d5f6e37ecbc
kajal kumari <kajal@ kumari>
2025-08-19 23:33:53 +0530

    Removed merge conflict markers from App.jsx

--- hiremi/src/App.jsx
+++ hiremi/src/App.jsx
@@ -5,33 +5,16 @@ import About from "./Pages/About";
 import Services from "./Pages/Services";
 import Contact from "./Pages/Contact";
 
-<<<<<<< HEAD
-
-import './App.css'
-import Navbar from './component/Navbar'
-import Home from './component/Home'
-import Careers from './component/Careers'
-import Footer from './component/Footer'
-=======
->>>>>>> kk
 function App() {
   return (
     <>
       <Navbar />
-<<<<<<< HEAD
-      <Home />
-      <Careers />
-      <Footer />
-      
-      
-=======
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
->>>>>>> kk
     </>
   );
 }
